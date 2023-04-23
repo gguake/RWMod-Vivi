cd809287117e4400989ef064936405b16ba0da28
gguake <gguak2@gmail.com>
2023-04-24 01:15:16 +0900

    프로폴리스 주입 추가, 알 온도 경고 추가, 브러쉬 색깔 수정

---
+++ Source/Egg/Alert_DangerViviEgg.cs
@@ -0,0 +1,80 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace VVRace
+{
+    public class Alert_DangerViviEgg : Alert
+    {
+        private List<Thing> dangerTargets = new List<Thing>();
+
+        private List<Thing> DangerTargets
+        {
+            get
+            {
+                dangerTargets.Clear();
+
+                var map = Find.CurrentMap;
+                if (map != null)
+                {
+                    foreach (var egg in map.listerThings.ThingsOfDef(VVThingDefOf.VV_ViviEgg))
+                    {
+                        var compTemperature = egg.TryGetComp<CompTemperatureRuinable>();
+                        if (compTemperature == null) { continue; }
+
+                        if (egg.AmbientTemperature > compTemperature.Props.maxSafeTemperature ||
+                            egg.AmbientTemperature < compTemperature.Props.minSafeTemperature)
+                        {
+                            dangerTargets.Add(egg);
+                        }
+                    }
+
+                    foreach (var building in map.listerBuildings.AllBuildingsColonistOfDef(VVThingDefOf.VV_ViviHatchery))
+                    {
+                        var hatchery = building as ViviEggHatchery;
+                        if (hatchery != null && hatchery.ViviEgg != null)
+                        {
+                            var egg = hatchery.ViviEgg;
+                            var compTemperature = egg.TryGetComp<CompTemperatureRuinable>();
+                            if (compTemperature == null) { continue; }
+
+                            if (egg.AmbientTemperature > compTemperature.Props.maxSafeTemperature ||
+                                egg.AmbientTemperature < compTemperature.Props.minSafeTemperature)
+                            {
+                                dangerTargets.Add(hatchery);
+                            }
+                        }
+
+                    }
+                }
+
+                return dangerTargets;
+            }
+        }
+
+        public Alert_DangerViviEgg()
+        {
+            defaultLabel = LocalizeTexts.AlertViviDangerousEggs.Translate();
+            defaultPriority = AlertPriority.High;
+        }
+
+        public override AlertReport GetReport()
+        {
+            var dangerTargets = DangerTargets;
+            if (dangerTargets.NullOrEmpty())
+            {
+                return false;
+            }
+
+            return AlertReport.CulpritsAre(dangerTargets);
+        }
+
+        public override TaggedString GetExplanation()
+        {
+            var props = VVThingDefOf.VV_ViviEgg.GetCompProperties<CompProperties_TemperatureRuinable>();
+            return LocalizeTexts.AlertViviDangerousEggsDesc.Translate(
+                props.minSafeTemperature.ToStringTemperature().Named("MINTEMP"), 
+                props.maxSafeTemperature.ToStringTemperature().Named("MAXTEMP"));
+        }
+    }
+}
---
+++ Source/Recipe_InjectPropolis.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace VVRace
+{
+    public class Recipe_InjectPropolis : Recipe_Surgery
+    {
+        public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
+        {
+            List<Hediff> targetHediffs = new List<Hediff>();
+            pawn.health.hediffSet.GetHediffs(ref targetHediffs, hediff =>
+            {
+                if (hediff.def == HediffDefOf.WoundInfection ||
+                    hediff.def == HediffDefOf.Flu ||
+                    hediff.def == HediffDefOf.Plague ||
+                    hediff.def == HediffDefOf.Malaria)
+                {
+                    return true;
+                }
+
+                return false;
+            });
+
+            foreach (var hediff in targetHediffs)
+            {
+                hediff.Severity = Mathf.Max(0.01f, hediff.Severity - 0.15f);
+            }
+        }
+    }
+}
