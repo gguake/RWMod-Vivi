dd727cfd885221c892ce4c6924c14e016e72ca24
gguake <gguak2@gmail.com>
2024-03-16 11:13:39 +0900

    1.4 last

---
+++ Source/EndGame/PeacebloomAltar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace VVRace
+{
+    public enum PeacebloomProjectStage
+    {
+        NotStarted,
+        InProgress,
+        Completed,
+    }
+
+    public class PeacebloomAltar : EnergyAcceptor
+    {
+        private bool _projectStarted;
+        private float _projectProgress;
+
+        public bool EnergySupplied => true;
+
+        public override EnergyFluxNetwork EnergyFluxNetwork { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override EnergyFluxNetworkNode EnergyFluxNode => throw new NotImplementedException();
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref _projectStarted, "projectStarted");
+            Scribe_Values.Look(ref _projectProgress, "projectProgress");
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+
+            if (EnergySupplied)
+            {
+            }
+        }
+    }
+}
--- make_directory_link.bat
+++ make_directory_link.bat
@@ -1,6 +1,13 @@
-@ECHO OFF
+@echo off
+
+setlocal
+for /f "tokens=*" %%a in ('cd') do set "current_directory=%%a"
+for %%I in ("%current_directory%") do set "folder_name=%%~nxI"
+
 IF "%RW_MODS_DIR%" == "" (
     @ECHO RW_MODS_DIR environment path is not exists.
 ) ELSE (
-    mklink /J %RW_MODS_DIR%\%1 Mod
+    mklink /J %RW_MODS_DIR%\%folder_name% Mod
 )
+
+endlocal
