2d8fe6cb72743c00881fc4e81309f6794bccafe9
gguake <gguak2@gmail.com>
2024-03-19 01:21:00 +0900

    RPEF로 변경

---
+++ Source/Vivi/PawnRenderNodeWorker_ViviCustomDrawSize.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace VVRace
+{
+    public class PawnRenderNodeProperties_ViviCustomDrawSize : PawnRenderNodeProperties_Parent
+    {
+        public float normalViviDrawSize;
+        public float royalViviDrawSize;
+    }
+
+    public class PawnRenderNodeWorker_ViviCustomDrawSize : PawnRenderNodeWorker
+    {
+        public override Vector3 ScaleFor(PawnRenderNode node, PawnDrawParms parms)
+        {
+            var scale = base.ScaleFor(node, parms);
+
+            var props = node.Props as PawnRenderNodeProperties_ViviCustomDrawSize;
+            if (props == null) { return scale; }
+
+            if (parms.pawn.IsVivi())
+            {
+                if (parms.pawn.IsRoyalVivi())
+                {
+                    scale *= props.royalViviDrawSize;
+                }
+                else
+                {
+                    scale *= props.normalViviDrawSize;
+                }
+            }
+
+            return scale;
+        }
+    }
+}
