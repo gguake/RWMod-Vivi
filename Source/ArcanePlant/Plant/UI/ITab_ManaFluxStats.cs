39f4a06f0aede6cdbc1f71af54811f41365c892d
gguake <gguak2@gmail.com>
2024-04-20 01:22:15 +0900

    버그 수정, 밸런스 조정

--- Source/ArcanePlant/Plant/UI/ITab_ManaFluxStats.cs
+++ Source/ArcanePlant/Plant/UI/ITab_ManaFluxStats.cs
@@ -11,13 +11,13 @@ namespace VVRace
 
         public ITab_ManaFluxStats()
         {
-            size = new Vector2(500f, 500f);
+            size = new Vector2(800f, 450f);
             labelKey = LocalizeString_ITab.VV_ITab_ManaAcceptor_StatsTabLabel.Translate();
         }
 
-        public override bool IsVisible => FluxNetwork != null && FluxNetwork.FluxHistory.Count > 0;
+        public override bool IsVisible => FluxNetwork != null && FluxNetwork.FluxHistory.Count > 1;
 
-        private static List<SimpleCurveDrawInfo> _tmpDrawInfoList;
+        private static List<SimpleCurveDrawInfo> _tmpDrawInfoList = new List<SimpleCurveDrawInfo>();
         protected override void FillTab()
         {
             var network = FluxNetwork;
@@ -27,6 +27,7 @@ namespace VVRace
             var legendRect = mainRect.NewRow(40f, VerticalJustification.Bottom);
             var graphRect = mainRect.Rect;
 
+            _tmpDrawInfoList.Clear();
             var generatedCurve = new SimpleCurveDrawInfo();
             generatedCurve.color = Color.green;
             generatedCurve.label = LocalizeString_ITab.VV_ITab_ManaAcceptor_ManaGenCurveLabel.Translate();
@@ -54,9 +55,9 @@ namespace VVRace
                 if (history.tick < minTick) { minTick = history.tick; }
                 if (history.tick > maxTick) { maxTick = history.tick; }
 
-                generatedCurve.curve.Add(new CurvePoint(history.tick / 60000f, history.generated), sort: false);
-                consumedCurve.curve.Add(new CurvePoint(history.tick / 60000f, history.consumed), sort: false);
-                exceededCurve.curve.Add(new CurvePoint(history.tick / 60000f, history.exceeded), sort: false);
+                generatedCurve.curve.Add(new CurvePoint(history.tick / 60000f, (int)(history.generated * GenTicks.TickRareInterval)), sort: false);
+                consumedCurve.curve.Add(new CurvePoint(history.tick / 60000f, (int)(history.consumed * GenTicks.TickRareInterval)), sort: false);
+                exceededCurve.curve.Add(new CurvePoint(history.tick / 60000f, (int)(history.exceeded * GenTicks.TickRareInterval)), sort: false);
             }
 
             generatedCurve.curve.SortPoints();
@@ -64,12 +65,12 @@ namespace VVRace
             exceededCurve.curve.SortPoints();
             if (network.FluxHistory.Count == 1)
             {
-                generatedCurve.curve.Add(new CurvePoint(1.6666667E-05f, network.FluxHistory.Peek().generated));
-                consumedCurve.curve.Add(new CurvePoint(1.6666667E-05f, network.FluxHistory.Peek().consumed));
-                exceededCurve.curve.Add(new CurvePoint(1.6666667E-05f, network.FluxHistory.Peek().exceeded));
+                generatedCurve.curve.Add(new CurvePoint(1.6666667E-05f, (int)(network.FluxHistory.Peek().generated * GenTicks.TickRareInterval)));
+                consumedCurve.curve.Add(new CurvePoint(1.6666667E-05f, (int)(network.FluxHistory.Peek().consumed * GenTicks.TickRareInterval)));
+                exceededCurve.curve.Add(new CurvePoint(1.6666667E-05f, (int)(network.FluxHistory.Peek().exceeded * GenTicks.TickRareInterval)));
             }
 
-            var section = new FloatRange(minTick, maxTick);
+            var section = new FloatRange(minTick / 60000f, maxTick / 60000f);
             if (Mathf.Approximately(section.min, section.max))
             {
                 section.max += 1.6666667E-05f;
