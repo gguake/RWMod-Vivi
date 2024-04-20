3f49feef5b8f67f5c877a95db377e715f3642a35
gguake <gguak2@gmail.com>
2024-04-21 02:39:59 +0900

    부화기 버그 수정

--- Source/Egg/Things/ViviEggHatchery.cs
+++ Source/Egg/Things/ViviEggHatchery.cs
@@ -40,8 +40,6 @@ namespace VVRace
                     return false;
                 }
 
-                if (Position.GetFirstPawn(Map) != null) { return false; }
-
                 return true;
             }
         }
