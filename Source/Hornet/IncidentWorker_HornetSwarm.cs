c24df2fc96310b8e4a8a783c2d32e3c0fc7f2756
gguake <gguak2@gmail.com>
2024-04-22 19:19:26 +0900

    말벌 확률 및 포인트 수정

--- Source/Hornet/IncidentWorker_HornetSwarm.cs
+++ Source/Hornet/IncidentWorker_HornetSwarm.cs
@@ -1,13 +1,38 @@
 ﻿using RimWorld;
+using System.Linq;
 using Verse;
 
 namespace VVRace
 {
     public class IncidentWorker_HornetSwarm : IncidentWorker
     {
-        protected virtual float PointsFactor => 1f;
+        private static SimpleCurve _curveChanceFactor = new SimpleCurve(new CurvePoint[]
+        {
+            new CurvePoint(0f, 1f),
+            new CurvePoint(2.999f, 1f),
+            new CurvePoint(3f, 3f),
+            new CurvePoint(10f, 3f),
+            new CurvePoint(20f, 5f),
+            new CurvePoint(100f, 7f),
+        });
+
+        private static SimpleCurve _curvePointFactor = new SimpleCurve(new CurvePoint[]
+        {
+            new CurvePoint(0f, 1f),
+            new CurvePoint(5f, 1f),
+            new CurvePoint(15f, 1.5f),
+            new CurvePoint(20f, 2.0f),
+            new CurvePoint(100f, 2.5f),
+        });
+
         private const int AnimalsStayDurationMin = 90000;
-        private const int AnimalsStayDurationMax = 240000;
+        private const int AnimalsStayDurationMax = 180000;
+
+        public override float ChanceFactorNow(IIncidentTarget target)
+        {
+            var viviCount = target.PlayerPawnsForStoryteller.Count(v => v.IsVivi());
+            return _curveChanceFactor.Evaluate(viviCount);
+        }
 
         protected override bool CanFireNowSub(IncidentParms parms)
         {
@@ -16,9 +41,11 @@ namespace VVRace
                 return false;
             }
 
-            if (parms.points * PointsFactor < 1500) { return false; }
-
             var map = (Map)parms.target;
+            var viviCount = map.PlayerPawnsForStoryteller.Count(v => v.IsVivi());
+            var points = parms.points * _curvePointFactor.Evaluate(viviCount);
+            if (points < 1500) { return false; }
+
             return RCellFinder.TryFindRandomPawnEntryCell(out _, map, CellFinder.EdgeRoadChance_Animal);
         }
 
@@ -33,7 +60,10 @@ namespace VVRace
                 return false;
             }
 
-            var pawns = AggressiveAnimalIncidentUtility.GenerateAnimals(kindDef, map.Tile, parms.points * PointsFactor, parms.pawnCount);
+            var viviCount = map.PlayerPawnsForStoryteller.Count(v => v.IsVivi());
+            var points = parms.points * _curvePointFactor.Evaluate(viviCount);
+
+            var pawns = AggressiveAnimalIncidentUtility.GenerateAnimals(kindDef, map.Tile, points, parms.pawnCount);
             var rot = Rot4.FromAngleFlat((map.Center - spawnPosition).AngleFlat);
 
             for (int i = 0; i < pawns.Count; i++)
@@ -54,22 +84,5 @@ namespace VVRace
 
     public class IncidentWorker_HornetSwarm_Vivi : IncidentWorker_HornetSwarm
     {
-        protected override float PointsFactor => 1.35f;
-
-        protected override bool CanFireNowSub(IncidentParms parms)
-        {
-            if (!base.CanFireNowSub(parms))
-            {
-                return false;
-            }
-
-            var map = (Map)parms.target;
-            if (!map.ParentFaction.IsPlayer || !map.ParentFaction.def.allowedCultures.Contains(VVCultureDefOf.VV_ViviCulture))
-            {
-                return false;
-            }
-
-            return true;
-        }
     }
 }
