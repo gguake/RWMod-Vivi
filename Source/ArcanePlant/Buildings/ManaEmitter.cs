6b406edaaabaea1f0bee05950cd648a9388cb9fa
gguake <gguak2@gmail.com>
2025-07-08 00:50:28 +0900

    마나 방출기

--- Source/ArcanePlant/Buildings/ManaEmitter.cs
+++ Source/ArcanePlant/Buildings/ManaEmitter.cs
@@ -1,9 +1,25 @@
-﻿using Verse;
+﻿using RimWorld;
+using Verse;
 
 namespace VVRace
 {
     public class ManaEmitter : Building
     {
+        public CompRefuelable CompRefuelable
+        {
+            get
+            {
+                if (_compRefuelable == null)
+                {
+                    _compRefuelable = GetComp<CompRefuelable>();
+                }
+                return _compRefuelable;
+            }
+        }
+        private CompRefuelable _compRefuelable;
+
+        public override int? OverrideGraphicIndex => CompRefuelable.HasFuel ? 1 : 0;
+
         public override int UpdateRateTicks => 1200;
         protected override int MaxTickIntervalRate => 1000;
     }
