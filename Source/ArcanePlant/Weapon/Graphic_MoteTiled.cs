10d48f883dbc27b794eead5655ce6eae8ad082d9
gguake <gguak2@gmail.com>
2025-07-10 03:14:35 +0900

    아크 피스톨 작업

---
+++ Source/ArcanePlant/Weapon/Graphic_MoteTiled.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Verse;
+
+namespace VVRace
+{
+    public class Graphic_MoteTiled : Graphic_Single
+    {
+        protected static MaterialPropertyBlock propertyBlock = new MaterialPropertyBlock();
+
+        protected static Dictionary<int, Mesh> _meshes = new Dictionary<int, Mesh>();
+
+        public override void DrawWorker(Vector3 loc, Rot4 rot, ThingDef thingDef, Thing thing, float extraRotation)
+        {
+            DrawMoteInternal(loc, rot, thingDef, thing, 0);
+        }
+
+        public void DrawMoteInternal(Vector3 loc, Rot4 rot, ThingDef thingDef, Thing thing, int layer)
+        {
+            DrawMote(data, MatSingle, base.Color, loc, rot, thingDef, thing, 0);
+        }
+
+        public static void DrawMote(GraphicData data, Material material, Color color, Vector3 loc, Rot4 rot, ThingDef thingDef, Thing thing, int layer)
+        {
+            var mote = (Mote)thing;
+            var alpha = mote.Alpha;
+            if (thing != null && alpha > 0f)
+            {
+                var multipliedColor = color * mote.instanceColor;
+                multipliedColor.a *= alpha;
+
+                var exactScale = mote.ExactScale;
+                exactScale.x *= data.drawSize.x;
+                exactScale.z *= data.drawSize.y;
+
+                var mesh = MakeUVScaledMesh(Mathf.Max(1, (int)(exactScale.z * 10)));
+
+                Matrix4x4 matrix = default;
+                matrix.SetTRS(mote.DrawPos, Quaternion.AngleAxis(mote.exactRotation, Vector3.up), exactScale);
+
+                material.mainTexture.wrapModeV = TextureWrapMode.Repeat;
+
+                propertyBlock.SetFloat(ShaderPropertyIDs.AgeSecs, mote.AgeSecs);
+                propertyBlock.SetFloat(ShaderPropertyIDs.AgeSecsPausable, mote.AgeSecsPausable);
+                propertyBlock.SetFloat(ShaderPropertyIDs.RandomPerObject, Gen.HashCombineInt(mote.spawnTick, mote.DrawPos.GetHashCode()));
+                propertyBlock.SetFloat(ShaderPropertyIDs.RandomPerObjectOffsetRandom, Gen.HashCombineInt(mote.spawnTick, mote.offsetRandom));
+                propertyBlock.SetColor(ShaderPropertyIDs.Color, multipliedColor);
+                Graphics.DrawMesh(mesh, matrix, material, layer, null, 0, propertyBlock);
+            }
+        }
+
+        private static Mesh MakeUVScaledMesh(int yScale)
+        {
+            if (_meshes.TryGetValue(yScale, out var mesh)) { return mesh; }
+
+            var actualScale = yScale / 20f;
+            var vtx = new Vector3[4];
+            var uvs = new Vector2[4];
+
+            var idx = new int[6];
+            vtx[0] = new Vector3(-0.5f, 0f, -0.5f);
+            vtx[1] = new Vector3(-0.5f, 0f, 0.5f);
+            vtx[2] = new Vector3(0.5f, 0f, 0.5f);
+            vtx[3] = new Vector3(0.5f, 0f, -0.5f);
+
+            uvs[0] = new Vector2(1f, 0f);
+            uvs[1] = new Vector2(1f, actualScale);
+            uvs[2] = new Vector2(0f, actualScale);
+            uvs[3] = new Vector2(0f, 0f);
+
+            idx[0] = 0;
+            idx[1] = 1;
+            idx[2] = 2;
+            idx[3] = 0;
+            idx[4] = 2;
+            idx[5] = 3;
+
+            mesh = new Mesh();
+            mesh.name = "NewPlaneMesh()";
+            mesh.vertices = vtx;
+            mesh.uv = uvs;
+            mesh.SetTriangles(idx, 0);
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+
+            _meshes.Add(yScale, mesh);
+            return mesh;
+        }
+    }
+}
---
+++ Source/ArcanePlant/Weapon/Verb_ChainLightning.cs
@@ -0,0 +1,187 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VVRace
+{
+    public class VerbProperties_ChainLightning : VerbProperties
+    {
+        public DamageDef damageDef;
+        public int? damageAmount;
+
+        public ThingDef lightningMoteDef;
+        public float lightningStartOffset;
+        public EffecterDef lightningTargetEffectDef;
+
+        public VerbProperties_ChainLightning()
+        {
+            verbClass = typeof(Verb_ChainLightning);
+        }
+    }
+
+    public class Verb_ChainLightning : Verb
+    {
+        public CompMana ManaComp
+        {
+            get
+            {
+                if (_manaComp == null)
+                {
+                    _manaComp = caster.TryGetComp<CompMana>();
+                }
+
+                if (_manaComp == null)
+                {
+                    _manaComp = EquipmentSource?.TryGetComp<CompMana>();
+                }
+
+                return _manaComp;
+            }
+        }
+        [Unsaved]
+        private CompMana _manaComp;
+
+        protected override int ShotsPerBurst => base.BurstShotCount;
+
+        public VerbProperties_ChainLightning VerbProps => (VerbProperties_ChainLightning)verbProps;
+
+        private IntVec3 _targetCell;
+        private Vector3 _targetPosition;
+
+        [Unsaved]
+        private MoteDualAttached _mote;
+        [Unsaved]
+        private Effecter _targetEffecter;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref _targetCell, "targetCell");
+            Scribe_Values.Look(ref _targetPosition, "targetPosition");
+        }
+
+        protected override bool TryCastShot()
+        {
+            if (currentTarget.HasThing && currentTarget.Thing.Map != caster.Map)
+            {
+                return false;
+            }
+
+            var compMana = ManaComp;
+            if (compMana == null) { return false; }
+
+            var manaPerShoot = EquipmentSource?.GetStatValue(VVStatDefOf.VV_RangedWeapon_ManaCost) ?? 0;
+            if (compMana.Stored < manaPerShoot) { return false; }
+
+            var los = TryFindShootLineFromTo(caster.Position, currentTarget, out var resultingLine);
+            if (verbProps.stopBurstWithoutLos && !los)
+            {
+                return false;
+            }
+
+            if (EquipmentSource != null)
+            {
+                EquipmentSource.GetComp<CompChangeableProjectile>()?.Notify_ProjectileLaunched();
+                EquipmentSource.GetComp<CompApparelReloadable>()?.UsedOnce();
+            }
+
+            lastShotTick = Find.TickManager.TicksGame;
+
+            if (currentTarget.HasThing)
+            {
+                var log = new BattleLogEntry_RangedImpact(caster, currentTarget.Thing, currentTarget.Thing, EquipmentSource.def, null, null);
+                var angleFlat = (currentTarget.Cell - caster.Position).AngleFlat;
+                var amount = (float)(VerbProps.damageAmount ?? VerbProps.damageDef.defaultDamage);
+
+                if (VerbProps.damageDef != null)
+                {
+                    var dinfo = new DamageInfo(
+                        VerbProps.damageDef,
+                        amount,
+                        VerbProps.damageDef.defaultArmorPenetration,
+                        angleFlat,
+                        caster,
+                        null,
+                        EquipmentSource.def,
+                        DamageInfo.SourceCategory.ThingOrUnknown,
+                        currentTarget.Thing);
+
+                    currentTarget.Thing.TakeDamage(dinfo).AssociateWithLog(log);
+                }
+
+                var empIinfo = new DamageInfo(
+                    DamageDefOf.EMP,
+                    amount * verbProps.burstShotCount,
+                    VerbProps.damageDef.defaultArmorPenetration,
+                    angleFlat,
+                    caster,
+                    null,
+                    EquipmentSource.def,
+                    DamageInfo.SourceCategory.ThingOrUnknown,
+                    currentTarget.Thing);
+
+                currentTarget.Thing.TakeDamage(empIinfo).AssociateWithLog(log);
+            }
+
+            compMana.Stored -= manaPerShoot;
+            return true;
+        }
+
+        public override void BurstingTick()
+        {
+            var vector = currentTarget.CenterVector3 - caster.Position.ToVector3Shifted();
+            var x = vector.MagnitudeHorizontal();
+            var normalized = vector.Yto0().normalized;
+
+            var offsetCaster = normalized * VerbProps.lightningStartOffset;
+            var offsetTarget = _targetPosition - _targetCell.ToVector3Shifted();
+
+            if (_mote != null)
+            {
+                _mote.UpdateTargets(caster, new TargetInfo(currentTarget.Cell, caster.Map), offsetCaster, offsetTarget);
+                _mote.Maintain();
+            }
+
+            if (_targetEffecter == null && VerbProps.lightningTargetEffectDef != null)
+            {
+                _targetEffecter = VerbProps.lightningTargetEffectDef.Spawn(_targetCell, caster.Map, offsetTarget);
+            }
+            if (_targetEffecter != null)
+            {
+                _targetEffecter.offset = offsetTarget;
+                _targetEffecter.EffectTick(new TargetInfo(_targetCell, caster.Map), TargetInfo.Invalid);
+                _targetEffecter.ticksLeft--;
+            }
+
+            if (verbProps.beamLineFleckDef != null)
+            {
+                float num2 = 1f * x;
+                for (int i = 0; (float)i < num2; i++)
+                {
+                    if (Rand.Chance(verbProps.beamLineFleckChanceCurve.Evaluate((float)i / num2)))
+                    {
+                        Vector3 vector4 = i * normalized - normalized * Rand.Value + normalized / 2f;
+                        FleckMaker.Static(caster.Position.ToVector3Shifted() + vector4, caster.Map, verbProps.beamLineFleckDef);
+                    }
+                }
+            }
+        }
+
+        public override void WarmupComplete()
+        {
+            burstShotsLeft = ShotsPerBurst;
+            state = VerbState.Bursting;
+
+            _mote = MoteMaker.MakeInteractionOverlay(
+                VerbProps.lightningMoteDef, 
+                caster, 
+                new TargetInfo(currentTarget.Cell, caster.Map));
+
+            TryCastNextBurstShot();
+
+            _targetCell = currentTarget.Cell;
+            _targetPosition = currentTarget.CenterVector3;
+        }
+    }
+}
