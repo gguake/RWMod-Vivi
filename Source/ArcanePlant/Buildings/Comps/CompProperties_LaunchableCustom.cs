ccb721951301dd7ec119ff073ec18f2adee67acd
gguake <gguak2@gmail.com>
2025-07-24 03:07:01 +0900

    하늘풀 상자 고장난것 수정

--- Source/ArcanePlant/Buildings/Comps/CompProperties_LaunchableCustom.cs
+++ Source/ArcanePlant/Buildings/Comps/CompProperties_LaunchableCustom.cs
@@ -19,5 +19,25 @@ namespace VVRace
     {
         public ThingDef activeDropPod;
         public ThingDef incomingDropPod;
+
+        public CompProperties_LaunchableCustom()
+        {
+            compClass = typeof(CompLaunchableCustom);
+        }
+    }
+
+    public class CompLaunchableCustom : CompLaunchable
+    {
+        public override bool RequiresFuelingPort => false;
+
+        public override CompRefuelable Refuelable => null;
+
+        public override float FuelLevel => 0;
+
+        public override float MaxFuelLevel => 0;
+
+        public CompLaunchableCustom()
+        {
+        }
     }
 }
