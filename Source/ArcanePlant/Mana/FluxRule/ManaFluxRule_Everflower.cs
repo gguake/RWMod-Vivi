d65477cf5cab6ccabec5bfc0f10649893b74bfc1
gguake <gguak2@gmail.com>
2025-07-28 14:48:45 +0900

    영원꽃 마나 조정

---
+++ Source/ArcanePlant/Mana/FluxRule/ManaFluxRule_Everflower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace VVRace
+{
+    public class ManaFluxRule_Everflower : ManaFluxRule
+    {
+        public List<IntRange> ranges;
+
+        public override IntRange FluxRangeForDisplay => new IntRange(-1, -1);
+
+        public override string GetRuleString() => $"+???";
+
+        public override int CalcManaFlux(Thing thing)
+        {
+            if (thing is ArcanePlant_Everflower everflower && everflower.EverflowerComp != null)
+            {
+                var level = everflower.EverflowerComp.AttunementLevel;
+                return ranges[Mathf.Min(ranges.Count - 1, level)].RandomInRange;
+            }
+
+            return 0;
+        }
+    }
+}
