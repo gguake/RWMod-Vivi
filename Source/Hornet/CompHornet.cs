a95e9b3db6d4c86c9ee47f8542f7ba58240ee779
gguake <gguak2@gmail.com>
2025-08-01 19:04:29 +0900

    말벌 조금 조정

---
+++ Source/Hornet/CompHornet.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VVRace
+{
+    public class CompHornet : ThingComp
+    {
+        public override void PostPreApplyDamage(ref DamageInfo dinfo, out bool absorbed)
+        {
+            base.PostPreApplyDamage(ref dinfo, out absorbed);
+
+            if (dinfo.Def.armorCategory.armorRatingStat == StatDefOf.ArmorRating_Heat)
+            {
+                dinfo.SetAmount(dinfo.Amount * 2f);
+            }
+        }
+    }
+}
