2acfa668bff2f899865b6cc5a98a72ba1b2ff82f
gguake <gguak2@gmail.com>
2026-06-19 12:04:16 +0900

    로열 비비 변화 조건 조정

--- Source/Vivi/Need_RoyalJelly.cs
+++ Source/Vivi/Need_RoyalJelly.cs
@@ -6,19 +6,35 @@ namespace VVRace
 {
     public class Need_RoyalJelly : Need
     {
-        public const float ThresholdBeRoyal = 0.999f;
+        public const float MinimumThresholdBeRoyal = 0.98f;
+        public const float FixedThresholdBeRoyal = 0.995f;
 
         public bool ShouldBeRoyalIfMature
         {
             get
             {
-                return CurLevelPercentage >= ThresholdBeRoyal;
+                var pct = CurLevelPercentage;
+                if (pct < MinimumThresholdBeRoyal) { return false; }
+                if (pct > FixedThresholdBeRoyal) { return true; }
+
+                var result = false;
+                try
+                {
+                    Rand.PushState(pawn.thingIDNumber);
+                    result = Rand.Chance((pct - MinimumThresholdBeRoyal) / (1f - MinimumThresholdBeRoyal));
+                }
+                finally
+                {
+                    Rand.PopState();
+                }
+
+                return result;
             }
         }
 
         public Need_RoyalJelly(Pawn pawn) : base(pawn)
         {
-            threshPercents = new List<float>() { ThresholdBeRoyal };
+            threshPercents = new List<float>() { 0.99f };
         }
 
         public override void SetInitialLevel()
